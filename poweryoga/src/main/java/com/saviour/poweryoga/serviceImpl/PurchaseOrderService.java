9ba0e35d2be220d25a2786967475fd7d6537ddec
shahin <shahin@10.10.54.161>
2014-08-04 21:15:42 -0700

    Search and Purchase Product

--- poweryoga/src/main/java/com/saviour/poweryoga/serviceImpl/PurchaseOrderService.java
+++ poweryoga/src/main/java/com/saviour/poweryoga/serviceImpl/PurchaseOrderService.java
@@ -6,16 +6,21 @@
 package com.saviour.poweryoga.serviceImpl;
 
 import com.saviour.poweryoga.daoI.IPurchaseOrderDAO;
+import com.saviour.poweryoga.model.Customer;
 import com.saviour.poweryoga.model.PurchaseOrder;
+import com.saviour.poweryoga.model.ShoppingCart;
 import com.saviour.poweryoga.serviceI.IPurchaseOrderService;
+import java.util.Calendar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Md Mojahidul Islam
  * @version 0.0.1
  */
+@Transactional
 @Service
 public class PurchaseOrderService implements IPurchaseOrderService {
 
@@ -23,7 +28,9 @@ public class PurchaseOrderService implements IPurchaseOrderService {
     private IPurchaseOrderDAO purchaseOrderDao;
 
     @Override
-    public void savePurchaseOrder(PurchaseOrder order) {
+    public PurchaseOrder savePurchaseOrder(ShoppingCart shoppingCart, Customer customer, Calendar buyDate) {
+        PurchaseOrder purchaseOrder = new PurchaseOrder(buyDate, shoppingCart, customer);
+        return purchaseOrderDao.savePurchaseOrder(purchaseOrder);
 
     }
 
